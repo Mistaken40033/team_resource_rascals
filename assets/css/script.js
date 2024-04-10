2a8d42c20c51912aa8b513196c7a65346b26b708
Nekoteej <161190070+Nekoteej@users.noreply.github.com>
2024-04-09 20:13:16 -0600

    javascript placeholders

---
+++ assets/css/script.js
@@ -0,0 +1,5 @@
+const gitAPI = //github key here
+const googAPI = //google api key here
+
+const gitInfoContainer = document.getElementById(//empty div on the html)
+const googInfoContainer = document.getElementById(//empty div on the html)
