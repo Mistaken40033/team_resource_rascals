c9d967053dccf69d2e321d1fab22ca7e88347489
Nekoteej <161190070+Nekoteej@users.noreply.github.com>
2024-04-09 20:47:36 -0600

    blank divs for appending on resource html and github key

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,4 +1,4 @@
-const gitAPI = //github key here
+const gitAPI = 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
 const googAPI = //google api key here
 
 const gitInfoContainer = document.getElementById(//empty div on the html)
