2dd3987a8b1dabb6bdfb240d9c133f05b0197273
Nekoteej <161190070+Nekoteej@users.noreply.github.com>
2024-04-09 21:12:29 -0600

    empty divs on resource html, and getting the in js

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,6 +1,6 @@
 const gitAPI = //github key here
 const googAPI = //google api key here
 
-const gitInfoContainer = document.getElementById(//empty div on the html)
-const googInfoContainer = document.getElementById(//empty div on the html)
+const gitInfoContainer = document.getElementById('gitPlace')
+const googInfoContainer = document.getElementById('googPlace')
 
